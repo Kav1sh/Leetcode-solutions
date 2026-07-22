9d30ab5c87a602812652e449327cd9b1524c352a
kav1sh <kavishghumre@gmail.com>
2026-07-22 14:48:15 +0530

    solved best time to buy and sell stock easy

---
+++ leetcode/easy/0009_besttimetobuyandsellstock.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l,r=0,1 #l = buy. r = sell. 
+        max_p=0
+        while r<(len(prices)):
+            if prices[l]<prices[r]:
+                profit = prices[r]-prices[l]
+                max_p = max(max_p,profit)
+                r+=1
+            else:
+                l=r
+                r+=1
+        return max_p  
