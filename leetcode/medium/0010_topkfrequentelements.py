5f2fa0d55b1085b924ce18affa9e7b439c6d92fc
kav1sh <kavishghumre@gmail.com>
2026-07-23 22:20:16 +0530

    solved top k frequent elements

---
+++ leetcode/medium/0010_topkfrequentelements.py
@@ -0,0 +1,20 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+            freq = {}
+
+        
+            for num in nums:
+                if num not in freq:
+                    freq[num] = 1
+                else:
+                    freq[num] += 1
+
+          
+            sorted_freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+            result = []
+
+            for num, count in sorted_freq[:k]:
+                result.append(num)
+
+            return result
